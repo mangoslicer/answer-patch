ede90dacccf967117bb2a33d9065ec4798c53241
mangoslicer <patelndipen@gmail.com>
2017-01-01 17:04:02 -0500

    Refactored auth service to include password-constraint verification, unit tests, and more comments

--- services/auth.go
+++ services/auth.go
@@ -4,16 +4,27 @@ import (
 	"errors"
 	"log"
 	"time"
+	"regexp"
 
 	jwt "github.com/dgrijalva/jwt-go"
-	"github.com/patelndipen/AP1/datastores"
-	"github.com/patelndipen/AP1/settings"
+	"github.com/mangoslicer/answer-patch/datastores"
+	"github.com/mangoslicer/answer-patch/settings"
 	"golang.org/x/crypto/bcrypt"
 )
 
+/**
+ * Constants for JSON Web Token configuration
+*/
 const (
-	JWTLife     = 72
-	StoreOffset = 60
+	JWTLife     = 72 // Represents the amount of hours until a JSON Web Token will expire
+	StoreOffset = 60 // Represents the amount of seconds beyond the token's expiration time that the token will be stored as invalid
+)
+
+/**
+ * Constants for password validity verification
+*/
+const (
+	PasswordLength = 6
 )
 
 type Token struct {
@@ -26,58 +37,92 @@ type AuthServices interface {
 	RefreshToken() (*Token, error)
 }
 
+/**
+ * AuthContext is a structure that is used by the request handlers to access data about the current user
+ * Upon login, the user is ...
+*/
 type AuthContext struct {
 	UserID     string
 	Exp        time.Time
 	TokenStore datastores.TokenStoreServices
 }
 
-var InternalErr = errors.New("Internal Error")
-
+/**
+ * Creates a new AuthContext struct with the provided TokenStore
+*/
 func NewAuthContext(ts datastores.TokenStoreServices) *AuthContext {
 	return &AuthContext{UserID: "", Exp: time.Time{}, TokenStore: ts}
 }
 
+/**
+ * Provides a JSON Web Token for authenticated users
+*/
 func (ac *AuthContext) Login(enteredPassword, hashedPassword string) (*Token, error) {
-	if !authenticate(hashedPassword, enteredPassword) {
+	if !isValidPassword(enteredPassword) {
+		return nil, errors.New("Invalid password recieved")
+	} else if !authenticate(hashedPassword, enteredPassword) {
 		return nil, errors.New("Credentials are incorrect")
 	}
 
 	return setTokenClaims(ac.UserID)
 }
 
+/**
+ * The token parameter is stored in the TokenStore for an amount of hours determined by the sum of the remaining token life and the Offset
+ * Tokens of logged-out users are stored until at least expiration such that the server can detect and block any HTTP requests with these tokens
+*/
 func (ac *AuthContext) Logout(signedToken string) error {
 
 	storeTime := int(ac.Exp.Sub(time.Now()).Seconds() + StoreOffset)
 
-	err := ac.TokenStore.StoreToken(ac.UserID, signedToken, storeTime)
-	if err != nil {
-		return InternalErr
+	// Checking if storeTime is greater than 0 prevents the storage of expired JSON Web Tokens
+	if storeTime > 0 {
+		err := ac.TokenStore.StoreToken(ac.UserID, signedToken, storeTime)
+		if err != nil {
+			return errors.New("Internal Error")
+		}
 	}
 
 	return nil
 }
 
+/**
+ * Rather than parsing, delaying the expiration time, and signing the current token, RefreshToken simply returns a new token
+*/
 func (ac *AuthContext) RefreshToken() (*Token, error) {
 	return setTokenClaims(ac.UserID)
 }
 
+/**
+ * Authentication of user is performed by comparing the hash of the entered password with the stored hash of the user's password
+*/
 func authenticate(hashedPassword, enteredPassword string) bool {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(enteredPassword)) == nil
-	return true
-
 }
 
+/**
+ * Initializes JSON Web Token with initialization time, expiration time, and the userID
+*/
 func setTokenClaims(userID string) (*Token, error) {
-	token := jwt.New(jwt.SigningMethodRS512)
-	token.Claims["iat"] = time.Now().Unix()
-	token.Claims["exp"] = time.Now().Add(time.Hour * time.Duration(JWTLife)).Unix()
-	token.Claims["sub"] = userID
+	token := jwt.NewWithClaims(jwt.SigningMethodRS512, jwt.MapClaims {
+		"iat": time.Now().Unix(),
+		"exp": time.Now().Add(time.Hour * time.Duration(JWTLife)).Unix(),
+		"sub": userID,
+  })
 
 	signedToken, err := token.SignedString(settings.GetPrivateKey())
 	if err != nil {
 		log.Fatal(err)
-		return nil, InternalErr
+		return nil, errors.New("Internal Error")
 	}
 	return &Token{SignedToken: signedToken}, nil
+}
+
+/**
+ * Checks whether password strings pass the character requirements of valid passwords
+*/
+func isValidPassword(password string) bool {
+	capitalLetterRegex, _ := regexp.Compile("[A-Z]+")  // Checks whether the password contains at least one capital letter
+	specialCharacterRegex, _ := regexp.Compile("\\W+") // Checks whether the password contains at least one special character
+	return len(password) >= PasswordLength && capitalLetterRegex.MatchString(password) && specialCharacterRegex.MatchString(password)
 }
--- services/auth_test.go
+++ services/auth_test.go
@@ -5,42 +5,108 @@ import (
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
-	"github.com/patelndipen/AP1/settings"
+	"github.com/mangoslicer/answer-patch/settings"
 )
 
+/**
+ * Mock of the datastore package's TokenStore struct
+ * The StoreTime field represents the amount of time for which a token of a logged-out user should be stored
+*/
 type MockTokenStore struct {
 	StoreTime int
 }
+
+/**
+ * Mock AuthContext available for testing functions, specifically the unit tests for the Login method
+*/
+var (
+	userID = "0" // Arbitrary userID for testing purposes
+	ac = &AuthContext{UserID: userID}
+)
 
 func init() {
 	settings.SetPreproductionEnv()
 }
 
+/**
+ * Mock implementation of StoreToken method from the TokenStoreServices interface
+ * Records the exp parameter for later use by TestLogoutStoreTimeCalculation
+*/
 func (store *MockTokenStore) StoreToken(key, val string, exp int) error {
-	store.StoreTime = exp //Records the exp param with which Logout called StoreToken in order for the TestLogoutStoreTimeCalculation func to test whether Logout calculated the correct StoreTime
+	store.StoreTime = exp
 	return nil
 }
 
+/**
+ * Mock implementation of StoreToken method from the TokenStoreServices interface
+ * IsTokenStored always returns false because
+*/
 func (store *MockTokenStore) IsTokenStored(key string) (bool, error) {
 	return false, nil
 }
 
+/**
+ * Tests that Login rejects passwords which do not meet the password requirements defined in auth.go
+*/
+func TestLoginWithInvalidPassword(t *testing.T) {
+	invalidPasswordMessage := "Login did not detect that %s is an invalid password was provided"
+  var invalidPasswords = []string {
+				"", // Does not pass the password length requirement
+				"no_capital_letters", // Lacks special character and capital letter
+				"Capital", //Lacks special character
+				"lowercase!", // Lacks capital letter
+	}
+	for _, invalidPassword := range invalidPasswords {
+	 		_, err := ac.Login(invalidPassword, "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824") //Arbitrary hash for testing purposes
+ 			if err.Error() != "Invalid password recieved"  {
+ 				t.Errorf(invalidPasswordMessage, invalidPassword)
+ 		}
+	}
+}
+
+/**
+ * Tests that Login accepts passwords which do meet the password requirements defined in auth.go
+*/
+func TestLoginWithValidPassword(t *testing.T) {
+	validPasswordMessage := "Login interpreted %s as an invalid password"
+	var validPasswords = []string {
+			"Abcde$",
+			"O'reillyAutoParts",
+	}
+ for _, validPassword := range validPasswords {
+		_, err := ac.Login(validPassword, "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824") //Arbitrary hash for testing purposes
+		if err.Error() == "Invalid password recieved"  {
+			t.Errorf(validPasswordMessage, validPassword)
+	}
+ }
+}
+
+/**
+ * Tests that the Login method correctly detects incorrect password and does not return a JSON Web Token
+*/
 func TestLoginWithIncorrectPassword(t *testing.T) {
-
-	ac := new(AuthContext)
-
-	_, err := ac.Login("incorrect password", "Hash")
+	_, err := ac.Login("incorrect password", "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824") // The password string does not match the hash string
 	if err == nil {
 		t.Errorf("Login did not detect that the provided password does not match the hashedpassword of the existing user.")
 	}
 }
 
+/**
+ * Tests that the Login method correctly detects password and does return a JSON Web Token
+ * Parses JSON Web Token to check whether the subscriber of the token matches the user ID of the user who requested the token
+*/
 func TestLoginWithCorrectPassword(t *testing.T) {
+ hashedPassword := "$2a$10$XgxVoZidTuxugFcAkBipBeSYxFJSLv/w0t1Lt7ihTOu0cThCBHgA." // Hash of "Passw!rd" string literal
 
-	ac := &AuthContext{UserID: "0"}
-	retrievedToken, err := ac.Login("password", "$2a$10$F4B95tmhW6VfQ.l.mhUj6Ow3Eg5dViTiJDsFzh8VaQr9Urd70LP9W")
+/*
+  userID :="0"
+	ac := &AuthContext{UserID: userID}
+*/
+	retrievedToken, err := ac.Login("Passw!rd", hashedPassword)
 	if err != nil {
 		t.Error(err)
+	} else if retrievedToken == nil {
+		t.Errorf("Correct password was provided, but the Login method did not return a JSON Web Token")
 	}
 
 	parsedToken, err := jwt.Parse(retrievedToken.SignedToken, func(token *jwt.Token) (interface{}, error) {
@@ -50,28 +116,33 @@ func TestLoginWithCorrectPassword(t *testing.T) {
 		t.Error(err)
 	}
 
-	parsedTokenUserID, ok := parsedToken.Claims["sub"].(string)
+	parsedTokenClaims, ok := parsedToken.Claims.(jwt.MapClaims)
 	if !ok {
-		t.Errorf("The parsedToken \"sub\" claim does not have an underlying type of string")
+		t.Errorf("Error reading parsed JSON Web Token")
 	}
 
-	if parsedTokenUserID != ac.UserID {
-		t.Errorf("Expected to retrieve a token with a \"sub\" claim of %s, but the retrieved token has a \"sub\" claim of %s", ac.UserID, parsedTokenUserID)
+	if parsedTokenClaims["sub"] != ac.UserID {
+		t.Errorf("Expected to retrieve a token with a \"sub\" claim of %s, but the retrieved token has a \"sub\" claim of %s", ac.UserID, parsedTokenClaims["sub"])
 	}
 }
 
+/**
+ * Tests whether the Logout method correctly calculates the time for which the token of a logged-out user should be stored
+*/
 func TestLogoutStoreTimeCalculation(t *testing.T) {
 
 	mockTokenStore := &MockTokenStore{StoreTime: 0}
-	ac := &AuthContext{Exp: time.Now(), TokenStore: mockTokenStore}
+	exp := time.Now().Add(time.Second * time.Duration(5)) // Time value 5 seconds ahead of current time
+	logoutAccountContext := &AuthContext{Exp: exp, TokenStore: mockTokenStore}
 
-	err := ac.Logout("Signed Token") // The Logout method's calculated storetime is recorded in mockTokenStore's StoreTime field by MockTokenStore's StoreToken method
+	signedToken := "WIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ" // Arbitrary signed JSON Web Token
+	err := logoutAccountContext.Logout(signedToken) // The Logout method's calculated storetime is recorded in mockTokenStore's StoreTime field by MockTokenStore's StoreToken method
 	if err != nil {
 		t.Error(err)
 	}
 
-	if mockTokenStore.StoreTime < (StoreOffset-5) && mockTokenStore.StoreTime != StoreOffset {
-		t.Errorf("Expected the calculated store time of the token to be either equal to StoreOffset or greater than StoreOffset - 5, because the AuthContext struct, with which the Logout method was called, contained an Exp field of value time.Now(), but the calculated store time passed to StoreToken by the Logout method was %d seconds", mockTokenStore.StoreTime)
-
+	maxBoundOnStoreTimeCalc := exp.Sub(time.Now()).Seconds() // Represents the maximum amount of error (~5 seconds) which is allowed by the Logout storetime calculation
+	if mockTokenStore.StoreTime > (int(maxBoundOnStoreTimeCalc) + StoreOffset)  {
+		t.Errorf("Expected the calculated store time of the logged-out user's token to be less five seconds, but the store time was %d", mockTokenStore.StoreTime)
 	}
 }
